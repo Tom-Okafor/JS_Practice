64a12ad8c85b2e97320b81d1c274bbee01ee8c04
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-15 14:24:53 +0100

    Add files via upload

---
+++ newJs.js
@@ -0,0 +1,8 @@
+const user = {
+  name: "Obi",
+  age: 29,
+  nationality: "Ghana",
+  bundle: "free"
+}
+
+alert(`Current User is ${user.age}-year-old ${user.name} from ${user.nationality} using the ${user.bundle} bundle.`)
