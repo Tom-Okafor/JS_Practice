5dd16ce16a0bb384431fcfea8083213ef58a2103
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-21 13:20:58 +0100

    Add files via upload

---
+++ js12.js
@@ -0,0 +1,11 @@
+// what kind of data is the arguments
+
+function findTypeOf(dataType) {
+    console.log(typeof dataType);
+}
+let foods = {
+    tubers: ["yam", "cocoyam", "cassava"],
+    snacks: ["biscuits", "sausages"]
+};
+
+findTypeOf(foods);
