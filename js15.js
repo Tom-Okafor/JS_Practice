90763a01812dc0760644a86fa6b2490adf1439a5
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-25 08:34:28 +0100

    Add files via upload

--- js15.js
+++ js15.js
@@ -9,11 +9,7 @@ function arrangeNumsInIncreasingOrder(numbers) {
         for (let t = 0; t < ARRAY_LENGTH; t++) {
             if (numbers[i] > numbers[t]) {
                 greaterThanCounter++;
-            }
-        }
-
-        for (let j = 0; j < ARRAY_LENGTH; j++) {
-            if (numbers[i] == numbers[j]) {
+            } else if (numbers[i] == numbers[t]) {
                 equalToCounter++;
             }
         }
@@ -31,21 +27,42 @@ function arrangeNumsInIncreasingOrder(numbers) {
 const randomNumbers = [5, 2, 9, 4];
 const moreRandomNumbers = [3, 7, 9, 4, 55, 84, 1, 8, 9, 3, 2, 9];
 
-let figures = arrangeNumsInIncreasingOrder(randomNumbers);
+function logNewArray(newArray) {
+    let figures = newArray;
 
-for (let i = 0; i < figures.length; i++) {
-    let newFigure = figures[i];
-    console.log(newFigure);
+    for (let i = 0; i < figures.length; i++) {
+        let newFigure = figures[i];
+        console.log(newFigure);
+    }
+    console.log("\n");
 }
 
-console.log("\n\n");
+logNewArray(arrangeNumsInIncreasingOrder(randomNumbers));
+logNewArray(arrangeNumsInIncreasingOrder(moreRandomNumbers));
 
-figures = arrangeNumsInIncreasingOrder(moreRandomNumbers);
+function arrangeNumsInDecreasingOrder(numbers) {
+    let newArray = [];
+    const ARRAY_LENGTH = numbers.length;
+    for (let i = 0; i < ARRAY_LENGTH; i++) {
+        let lessThanCounter = 0;
+        let equalToCounter = 0;
+        for (let t = 0; t < ARRAY_LENGTH; t++) {
+            if (numbers[i] < numbers[t]) {
+                lessThanCounter++;
+            } else if (numbers[i] == numbers[t]) {
+                equalToCounter++;
+            }
+        }
 
-for (let i = 0; i < figures.length; i++) {
-    let newFigure = figures[i];
-    console.log(newFigure);
+        newArray[lessThanCounter] = numbers[i];
+
+        while (equalToCounter) {
+            newArray[lessThanCounter++] = numbers[i];
+            equalToCounter--;
+        }
+    }
+    return newArray;
 }
 
-
-function arrangeNumsInDecreasingOrder(numbers){}
+logNewArray(arrangeNumsInDecreasingOrder(randomNumbers));
+logNewArray(arrangeNumsInDecreasingOrder(moreRandomNumbers));
