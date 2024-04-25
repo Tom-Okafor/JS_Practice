2e3cc4c2c3bc321bb19898443ce9194807fb9afa
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-25 16:56:30 +0100

    Add files via upload
    Refactorized code.

--- js15.js
+++ js15.js
@@ -1,68 +1,54 @@
 // Rearrange an array of numbers in increasing and decreasing order
-
 function arrangeNumsInIncreasingOrder(numbers) {
+    return createNewArray(numbers, 1);
+}
+function arrangeNumsInDecreasingOrder(numbers) {
+    return createNewArray(numbers, 0);
+}
+function createNewArray(numbers, condition) {
     let newArray = [];
     const ARRAY_LENGTH = numbers.length;
     for (let i = 0; i < ARRAY_LENGTH; i++) {
-        let greaterThanCounter = 0;
+        let comparisonCounter = 0;
         let equalToCounter = 0;
         for (let t = 0; t < ARRAY_LENGTH; t++) {
-            if (numbers[i] > numbers[t]) {
-                greaterThanCounter++;
-            } else if (numbers[i] == numbers[t]) {
-                equalToCounter++;
+            function checkEquality() {
+                if (numbers[i] == numbers[t]) {
+                    equalToCounter++;
+                }
+            }
+            if (condition < 1) {
+                if (numbers[i] < numbers[t]) {
+                    comparisonCounter++;
+                }
+                checkEquality();
+            } else {
+                if (numbers[i] > numbers[t]) {
+                    comparisonCounter++;
+                }
+                checkEquality();
             }
         }
-
-        newArray[greaterThanCounter] = numbers[i];
-
+        newArray[comparisonCounter] = numbers[i];
         while (equalToCounter) {
-            newArray[greaterThanCounter++] = numbers[i];
+            newArray[comparisonCounter++] = numbers[i];
             equalToCounter--;
         }
     }
     return newArray;
 }
-
-const randomNumbers = [5, 2, 9, 4];
-const moreRandomNumbers = [3, 7, 9, 4, 55, 84, 1, 8, 9, 3, 2, 9];
-
 function logNewArray(newArray) {
     let figures = newArray;
-
     for (let i = 0; i < figures.length; i++) {
         let newFigure = figures[i];
         console.log(newFigure);
     }
     console.log("\n");
 }
-
+const randomNumbers = [5, 2, 9, 4];
+const moreRandomNumbers = [3, 7, 9, 4, 55, 84, 1, 8, 9, 3, 2, 9];
 logNewArray(arrangeNumsInIncreasingOrder(randomNumbers));
 logNewArray(arrangeNumsInIncreasingOrder(moreRandomNumbers));
-
-function arrangeNumsInDecreasingOrder(numbers) {
-    let newArray = [];
-    const ARRAY_LENGTH = numbers.length;
-    for (let i = 0; i < ARRAY_LENGTH; i++) {
-        let lessThanCounter = 0;
-        let equalToCounter = 0;
-        for (let t = 0; t < ARRAY_LENGTH; t++) {
-            if (numbers[i] < numbers[t]) {
-                lessThanCounter++;
-            } else if (numbers[i] == numbers[t]) {
-                equalToCounter++;
-            }
-        }
-
-        newArray[lessThanCounter] = numbers[i];
-
-        while (equalToCounter) {
-            newArray[lessThanCounter++] = numbers[i];
-            equalToCounter--;
-        }
-    }
-    return newArray;
-}
 
 logNewArray(arrangeNumsInDecreasingOrder(randomNumbers));
 logNewArray(arrangeNumsInDecreasingOrder(moreRandomNumbers));
