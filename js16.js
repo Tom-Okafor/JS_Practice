97daff044c8a475239051f23847ad10b110e2628
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-27 21:14:04 +0100

    Add files via upload
    Made necessary corrections

--- js16.js
+++ js16.js
@@ -3,13 +3,21 @@
 // find the largest number from an array of random numbers
 
 const RANDOM_NUMBERS = [6, 7, 26, 91, 22, 62, 372, 2];
+const RANDOM_NUMBERS1 = [26, 17, 256, 91, 242, 62, 372, 5262];
+const RANDOM_NUMBERS2 = [611, 755, 126, 921, 252, 862, 37201, -20000];
+const RandomNUMBERS = [-0.1 - 2, -0.006, -0.0000099999, -9991];
 
 function findTheLargestNumber(numbers) {
     let greatestNumber = -9999999999999999999999;
-    for (let randomNumber of RANDOM_NUMBERS) {
+    for (let randomNumber of numbers) {
         if (greatestNumber < randomNumber) {
             greatestNumber = randomNumber;
         }
     }
     console.log(greatestNumber);
 }
+
+findTheLargestNumber(RANDOM_NUMBERS);
+findTheLargestNumber(RANDOM_NUMBERS1);
+findTheLargestNumber(RANDOM_NUMBERS2);
+findTheLargestNumber(RandomNUMBERS);
