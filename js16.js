f80c21a4b85e36e381a8f5482a6cc48e87c814cf
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-27 17:42:45 +0100

    Add files via upload

---
+++ js16.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// find the largest number from an array of random numbers
+
+const RANDOM_NUMBERS = [6, 7, 26, 91, 22, 62, 372, 2];
+
+function findTheLargestNumber(numbers) {
+    let greatestNumber = -9999999999999999999999;
+}
