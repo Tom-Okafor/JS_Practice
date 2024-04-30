9bf15065e93ee909f13a3b819e5ab83a530b56fc
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-30 16:41:13 +0100

    Add files via upload

---
+++ js18.js
@@ -0,0 +1,8 @@
+//create a recursive function that returns the fibionacci sequece
+
+function createFibonacciSequence(layers) {
+    let entryNumber = 0;
+    let newNumber = 1;
+    console.log(newNumber)
+    function returnFibonacciSequence() {}
+}
