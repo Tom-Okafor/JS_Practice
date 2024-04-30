d2885fcbe1ddd8c55a5d19d7935adcc06cd3f86d
TomOkafor <59459410+Tom-Okafor@users.noreply.github.com>
2024-04-30 17:17:38 +0100

    Add files via upload

--- js18.js
+++ js18.js
@@ -1,8 +1,28 @@
 //create a recursive function that returns the fibionacci sequece
 
-function createFibonacciSequence(layers) {
-    let entryNumber = 0;
+let FIBONACCI_ARRAY = [];
+
+function createFibonacciSequence(layers, entryNumber) {
+    /* let entryNumber = 0;
     let newNumber = 1;
-    console.log(newNumber)
+    console.log(newNumber);
     function returnFibonacciSequence() {}
+    */
+    if (FIBONACCI_ARRAY.length == 0) {
+        FIBONACCI_ARRAY.push(entryNumber);
+    } else if (FIBONACCI_ARRAY.length == 1) {
+        FIBONACCI_ARRAY.push(FIBONACCI_ARRAY[0]);
+    } else {
+        FIBONACCI_ARRAY.push(
+            FIBONACCI_ARRAY[FIBONACCI_ARRAY.length - 1] +
+                FIBONACCI_ARRAY[FIBONACCI_ARRAY.length - 2]
+        );
+    }
+    console.log(FIBONACCI_ARRAY[FIBONACCI_ARRAY.length - 1]);
+    layers--;
+    if (layers) {
+        createFibonacciSequence(layers, entryNumber);
+    }
 }
+
+createFibonacciSequence(10, 1);
